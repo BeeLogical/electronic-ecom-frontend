373f16e1d95100322af09b8c777cafb0aa123412
BeeLogical <101624558+BeeLogical@users.noreply.github.com>
2025-08-21 12:56:08 +0530

    Update server.js

--- electro-ecom/server.js
+++ electro-ecom/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,9 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle Angular routing - serve index.html for all non-API routes
-//app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, 'dist/electro-ecom/index.html'));
-//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
